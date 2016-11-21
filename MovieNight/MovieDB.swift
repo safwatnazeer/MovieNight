d2999692d6174d76e4910cb6a1a47c78a049c341
Safwat Shenouda <bingoapps@hotmail.com>
2016-11-21 13:17:40 +0100

    Initial commit to Github

--- MovieNight/MovieDB.swift
+++ MovieNight/MovieDB.swift
@@ -138,3 +138,42 @@ class APIClient {
     }
 
 }
+/*
+Query url:
+- search for an actor:
+https://api.themoviedb.org/3/search/person?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&language=en-US&query=%22Tom%20Hanks%22
+
+- Get list of movies for a year , but year doesnt work very well
+ https://api.themoviedb.org/3/discover/movie?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&year=2001
+
+- Get list of movies for a genre 
+ https://api.themoviedb.org/3/discover/movie?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&with_genres=28&page=1
+
+- Get list geners 
+ https://api.themoviedb.org/3/genre/movie/list?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&language=en-US
+
+- Get list of images for a movie 
+https://api.themoviedb.org/3/movie/188927/images?api_key=ae0b9efa77149c7c5c55edae3d42c5a9
+ 
+ 
+ 
+ 
+ 
+- Get image for a movie based on image url
+https://image.tmdb.org/t/p/w300/lQtNOJVScqqYhapr80JYUnChQeg.jpg
+ 
+- Get list of movies for an actor based on person id
+ https://api.themoviedb.org/3/person/31/movie_credits?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&language=en-US
+ 
+ - get list of movies for actor and year providing you the actor id
+ ----be careful to use primary_release_year instead of year---
+ 
+ https://api.themoviedb.org/3/discover/movie?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&with_cast=31&year=2010
+ 
+ - same but with sort based on poularity
+ https://api.themoviedb.org/3/discover/movie?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&with_cast=31&year=2010&sort_by=popularity.desc
+ 
+ - adding genre to discover
+ https://api.themoviedb.org/3/discover/movie?api_key=ae0b9efa77149c7c5c55edae3d42c5a9&primary_release_year=2013&sort_by=popularity.desc&with_genres=28
+ 
+ */
